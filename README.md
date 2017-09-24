aef3b281091a8a7857f0ed385327e562c3b6f62c
Petter Rodhelind <petter.rodhelind@gmail.com>
2017-09-24 21:09:47 +0200

    Initial commit: complete web server.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # www
 Quick and dirty local http server.
+
+Listens on port 8000 and serves from current directory.
+
+## Why
+
+I just wanted an easy command to serve up a local folder during quick fixes in web development. And I like Go.
---
+++ www.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	hostport := ":8000"
+	fmt.Println("Listening on", hostport)
+	err := http.ListenAndServe(hostport, logWrap(http.FileServer(http.Dir("."))))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func logWrap(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		defer func() {
+			fmt.Printf("%s: %s %v\n", r.Method, r.URL.Path, time.Since(t))
+		}()
+		h.ServeHTTP(w, r)
+	})
+}
